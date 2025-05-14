7462bd8b5915b2375cf4a4f7aebde361e398cf69
eirikpa <156180377+eirikpa@users.noreply.github.com>
2025-05-14 14:27:16 +0200

    Update f24-loop.sh

--- f24-loop.sh
+++ f24-loop.sh
@@ -3,9 +3,6 @@
 # This script simulates pressing Ctrl after a physical display has been confirmed connected, checking every 30 seconds.
 # This will trigger the cursor to hide (using HideAway) if a display with resolution higher than 1920x1080 is connected to the Pi after it has booted.
 
-# Path to wtype binary
-WTYPE_CMD="/usr/bin/wtype"
-
 # Wait until HDMI-A-1 or HDMI-A-2 shows up in wlr-randr output
 echo "Waiting for HDMI-A-1 or HDMI-A-2..."
 
@@ -14,4 +11,4 @@ while ! wlr-randr | grep -q '^HDMI-A-[12]'; do
 done
 
 echo "Display detected on HDMI-A-1 or HDMI-A-2. Sending F24 to hide cursor..."
-$WTYPE_CMD -M ctrl -m ctrl
+wtype -M ctrl -k R -m ctrl
