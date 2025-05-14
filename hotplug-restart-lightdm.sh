3bc05a07fcb35b193e991a7dcb259abd9599bf31
eirikpa <156180377+eirikpa@users.noreply.github.com>
2025-05-14 10:23:51 +0200

    Delete hotplug-restart-lightdm.sh

--- hotplug-restart-lightdm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-STATUS_FILE="/home/loopsign/screen-status.txt"
-
-# Check if the file exists and contains "(null)"
-if [[ -f "$STATUS_FILE" && "$(cat "$STATUS_FILE")" == "(null)" ]]; then
-    systemctl restart lightdm
-fi
