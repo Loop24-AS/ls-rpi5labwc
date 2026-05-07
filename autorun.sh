867332cb380b71802b686f5b900acc3bbcbcb201
eirikpa <156180377+eirikpa@users.noreply.github.com>
2026-05-07 12:05:52 +0200

    Add cache-refresh.sh to executable scripts

--- autorun.sh
+++ autorun.sh
@@ -153,8 +153,9 @@ start_countdown
 
 # Run the updated scripts
 cd /home/loopsign/ls-rpi5labwc
-chmod +x autorefresh.sh loopsign.sh hidecursor.sh loopsignsplash.sh pishrink.sh hideaway-trigger.sh configure-kanshi.sh # Adjust filenames as needed
+chmod +x autorefresh.sh loopsign.sh hidecursor.sh loopsignsplash.sh pishrink.sh hideaway-trigger.sh configure-kanshi.sh cache-refresh.sh # Adjust filenames as needed
 ./configure-kanshi.sh
 nohup ./autorefresh.sh &
+nohup ./cache-refresh.sh &
 ./loopsign.sh &
 ./hideaway-trigger.sh &
